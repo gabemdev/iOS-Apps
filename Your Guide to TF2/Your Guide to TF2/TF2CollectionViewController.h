facf66b6839b140db022d88c184e8f33edce6551
Varun Iyer <varun_iyer@icloud.com>
2013-12-25 05:53:23 -0500

    Changed some stuff
    stuff changed

---
+++ Your Guide to TF2/Your Guide to TF2/TF2CollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  TF2CollectionViewController.h
+//  Your Guide to TF2
+//
+//  Created by Varun Iyer on 12/25/13.
+//  Copyright (c) 2013 MoAppsCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TF2CollectionViewController : UICollectionViewController
+
+@end
---
+++ Your Guide to TF2/Your Guide to TF2/TF2CollectionViewController.m
@@ -0,0 +1,40 @@
+//
+//  TF2CollectionViewController.m
+//  Your Guide to TF2
+//
+//  Created by Varun Iyer on 12/25/13.
+//  Copyright (c) 2013 MoAppsCo. All rights reserved.
+//
+
+#import "TF2CollectionViewController.h"
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+@interface TF2CollectionViewController ()
+
+@end
+
+@implementation TF2CollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
