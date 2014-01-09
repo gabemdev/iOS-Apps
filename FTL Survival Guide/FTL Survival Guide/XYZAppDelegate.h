c94706ffc699fb7a67d054da846e7b57be003811
patelrrohan <patelrrohan@gmail.com>
2014-01-09 09:08:31 -0500

    Moved some stuff around and added some pics
    Got rid of an unnecessary folder and added some tentative pics that we
    can use for the icons, sidebar, and splash page. The pics are just in a
    folder, not on the actual app.

---
+++ FTL Survival Guide/FTL Survival Guide/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  FTL Survival Guide
+//
+//  Created by Rohan on 1/9/14.
+//  Copyright (c) 2014 Rohan Patel, Nishanth Shanmugham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FTL Survival Guide/FTL Survival Guide/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FTL Survival Guide
+//
+//  Created by Rohan on 1/9/14.
+//  Copyright (c) 2014 Rohan Patel, Nishanth Shanmugham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
